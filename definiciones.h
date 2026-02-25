dc7d7dbd65e3c8d51f8507045aa7a1badd1f9d05
Salvia <ladysalvitachan@gmail.com>
2026-02-25 17:20:25 +0100

    rematado ficheiro de definicions

--- definiciones.h
+++ definiciones.h
@@ -1,4 +1,12 @@
 #define IMPORT 300  // <300, "import">
 #define ID 301      //<301, "std">; <301, "stdio">
-
+#define DOUBLE 302
+#define STR_LITERAL 303
+#define INT_LITERAL 304 // Tamen inclúe os entriros binarios (0b...)
+#define FLOAT_LITERAL 305
+#define WHILE 306
+#define FOREACH 307
+#define RETURN 308
+#define VOID 309
+#define CAST 310
 
